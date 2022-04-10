75e5499388f54f9cfb8ad533b50e2879892d67c9
Sasha Chekh <cheh170804@gmail.com>
2022-04-10 16:32:58 +0300

    second commit

--- js/main.js
+++ js/main.js
@@ -5,20 +5,33 @@ window.addEventListener("DOMContentLoaded", () => {
 
   input.addEventListener("input", (e) => {
     if (+e.target.value[0] === 4) {
-      input.classList.add("input_green");
+      e.target.classList.add("input_green");
       icon.classList.add("active");
       icon.src = "./icons/visa.svg";
       icon.style.display = "block";
     } else if (+e.target.value[0] === 5) {
-      input.classList.add("input_green");
+      e.target.classList.add("input_green");
       icon.classList.add("active");
       icon.src = "./icons/mastercard.svg";
       icon.style.display = "block";
     } else {
+      e.target.classList.add("input_red")
       icon.style.display = "none";
     }
   });
+
   input.addEventListener("keydown", (e) => {
+    
+    if(e.key === "Backspace" && input.value.length - 2 <= 0 ) {
+      // console.log(true)
+      // console.log(input.value.length)
+      // console.log(e.target.value)
+      console.log(e.target.value)
+      console.log(e.target.value.length)
+      input.classList.remove("input_red")
+      input.classList.remove("input_green")
+    }
+
     if (
       e.key !== "Backspace" &&
       (e.target.value.length === 4 ||
@@ -31,6 +44,7 @@ window.addEventListener("DOMContentLoaded", () => {
       e.target.value = e.target.value.slice(0, 18);
     }
   });
+
   expiryDate.addEventListener("keydown", (e) => {
     e.target.value = e.target.value.slice(0, 4);
 
